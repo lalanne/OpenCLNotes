e816fbe3542d1e2d31b15faec0d3585784439b71
Christian Lalanne <christian.lalanne@ichec.ie>
2014-08-30 15:10:27 +0100

    adding new listing for more work kernel

---
+++ src/code/more_work.c
@@ -0,0 +1,16 @@
+__kernel 
+void more_work(__global float* a, __global float* b, __global float* output)                                                     
+{
+    int r = get_global_id(0);
+    int c, index;
+    float running;
+    int rank = get_global_size(0);
+
+    for (c=0; c < rank; c++) {
+        running  = 0.0f;
+        for(index = 0; index <  rank; index++)
+            running +=  a[r*rank+index] * b[index*rank+c];
+        output[r*rank + c] = running;
+    }   
+    return;
+}
